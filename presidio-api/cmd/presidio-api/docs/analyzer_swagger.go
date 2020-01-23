974eddb5d42b5d868aa99626db9a078554702a5b
Ubuntu <azure@dev.1y2omdlgywxefngq1jguxv1lsg.fx.internal.cloudapp.net>
2020-01-23 12:58:17 +0000

    swagger sepc

--- presidio-api/cmd/presidio-api/docs/analyzer_swagger.go
+++ presidio-api/cmd/presidio-api/docs/analyzer_swagger.go
@@ -9,7 +9,7 @@ import types "github.com/Microsoft/presidio-genproto/golang"
 // responses:
 //   200: recognizersGetResponse
 
-// A response includes a list of recognized fields in the text to be anonymized.
+// A Regognizer
 // swagger:response recognizersGetResponse
 type regognizerResponseWrapper struct {
 	// in:body
@@ -23,4 +23,46 @@ type regognizerParamsWrapper struct {
 	// in: path
 	// required: true
 	Id string `json:"id"`
+}
+
+// swagger:route GET /analyzer/recognizers/ analyzer getAllRecognizers
+//
+// Get all recognizers
+//
+// responses:
+//   200: []allRecognizersGetResponse
+
+// A a list of all recognizers
+// swagger:response allRecognizersGetResponse
+type allRegognizerResponseWrapper struct {
+	// in:body
+	Body []types.RecognizersGetResponse
+}
+
+// swagger:route POST /analyzer/recognizers/{id} analyzer insertRecognizer
+//
+// Insert a new recognizer
+//
+// responses:
+//   200: insertRecognizersResponse
+
+// Insert new recognizer response
+// swagger:response insertRecognizersResponse
+type insertRegognizerResponseWrapper struct {
+	// in: body
+	Body string
+}
+
+// swagger:parameters insertRecognizer
+type insertRegognizerParamsWrapper struct {
+	// Recognizer name
+	//
+	// in: path
+	// required: true
+	Id string `json:"id"`
+
+	// Recognizer properties
+	//
+	// in: body
+	Body types.RecognizerInsertOrUpdateRequest
 }
